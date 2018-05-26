2fd0b9b875a187414031f195671073a7d762e772
Gxiner <30684246+ZhangGuangXins@users.noreply.github.com>
2018-05-26 20:41:55 +0800

    first commit 1

---
+++ src/components/layouts.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container" > <!--最外层容器-->
+    <section class="menu"> <!--左边的容器-->
+      <menus></menus>
+    </section>
+    <section class="content-container"><!--右边的容器-->
+      <todo></todo>
+    </section>
+  </section>
+</template>
+<script>
+import menus from './menus';
+import todo from './todo';
+export default {
+    components: {
+        menus,
+        todo
+    }
+}
+</script>
+<style lang = "less" scoped>
+/* lang 指额外表示支持的语言可以让编辑器识别,scoped 指这里写的css只适用于该组件 */
+@import '../common/style/layouts.less';
+</style>
---
+++ src/components/menus.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list-todos">  <!--菜单容器-->
+      <a class="list-todo activeListClass list" v-for="item in items"> <!--单个菜单容器-->
+        <span class="icon-lock" v-if="item.locked"></span>
+        <span class="count-list" v-if="item.count">{{item.count}}</span>
+        {{item.title}}
+      </a>
+      <a class=" link-list-new" > <!--新增菜单-->
+        <span class="icon-plus"></span>新增
+      </a>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          { title: '星期一', count: 1, locked: true},
+          { title: '星期二', count: 1, locked: true},
+          { title: '星期三', count: 1, locked: true},
+          { title: '星期四', count: 1, locked: true},
+          { title: '星期五', count: 1, locked: true},
+          { title: '星期六', count: 1, locked: true},
+          { title: '星期日', count: 1, locked: true}
+        ]
+      }
+    }
+  };
+</script>
+<style lang="less">
+@import '../common/style/menu.less';
+</style>
